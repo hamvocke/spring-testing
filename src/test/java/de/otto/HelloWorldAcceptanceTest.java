431a68f25ded08fec4d4a58190fe6bf4f11bb907
Hermann Vocke <hvocke@thoughtworks.com>
2017-05-11 17:00:03 +0200

    add acceptance test using RestAssured

--- src/test/java/de/otto/HelloWorldAcceptanceTest.java
+++ src/test/java/de/otto/HelloWorldAcceptanceTest.java
@@ -2,22 +2,25 @@ package de.otto;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.core.Is.is;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloWorldAcceptanceTest {
+
+    @LocalServerPort
+    int port;
 
     @Test
     public void shouldReturnHelloWorld() throws Exception {
         when()
-                .get("http://localhost:8080/hello")
+                .get(String.format("http://localhost:%s/hello", port))
         .then()
                 .statusCode(is(200))
                 .body(containsString("Hello World!"));
