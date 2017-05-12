af8d72b773dc7715cd9117b506611561510ff9da
Hermann Vocke <hvocke@thoughtworks.com>
2017-05-12 15:30:07 +0200

    lookup person by last name, add unit test

--- src/main/java/example/HelloWorldController.java
+++ src/main/java/example/HelloWorldController.java
@@ -1,14 +1,31 @@
 package example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class HelloWorldController {
+
+    private final PersonRepository personRepository;
+
+    @Autowired
+    public HelloWorldController(final PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
     }
 
+    public String hello(final String lastName) {
+        Optional<Person> foundPerson = personRepository.findByLastName(lastName);
+
+        return foundPerson
+                .map(person -> String.format("Hello %s %s!", person.firstName, person.lastName))
+                .orElse(String.format("Who is this '%s' you're talking about?", lastName));
+    }
 }
--- src/test/java/example/HelloWorldControllerTest.java
+++ src/test/java/example/HelloWorldControllerTest.java
@@ -1,18 +1,52 @@
 package example;
 
-import example.HelloWorldController;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+
+import java.util.Optional;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class HelloWorldControllerTest {
 
-    private final HelloWorldController subject = new HelloWorldController();
+    private HelloWorldController subject;
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        initMocks(this);
+        subject = new HelloWorldController(personRepository);
+    }
 
     @Test
     public void shouldReturnHelloWorld() throws Exception {
         assertThat(subject.hello(), is("Hello World!"));
+    }
+
+    @Test
+    public void shouldReturnFullNameOfAPerson() throws Exception {
+        Person ham = new Person("Ham", "Vocke");
+        given(personRepository.findByLastName("Vocke")).willReturn(Optional.of(ham));
+
+        String greeting = subject.hello("Vocke");
+
+        assertThat(greeting, is("Hello Ham Vocke!"));
+    }
+
+    @Test
+    public void shouldTellIfPersonIsUnknown() throws Exception {
+        given(personRepository.findByLastName(anyString())).willReturn(Optional.empty());
+
+        String greeting = subject.hello("Vocke");
+
+        assertThat(greeting, is("Who is this 'Vocke' you're talking about?"));
     }
 
 }
