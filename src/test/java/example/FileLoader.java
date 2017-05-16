87d72280cb20b9faec0345ad0c1a2b508d75873e
Hermann Vocke <hvocke@thoughtworks.com>
2017-05-16 15:26:12 +0200

    load dummy json response from classpath

---
+++ src/test/java/example/FileLoader.java
@@ -0,0 +1,14 @@
+package example;
+
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class FileLoader {
+    public static String read(String filePath) throws IOException {
+        File file = ResourceUtils.getFile(filePath);
+        return new String(Files.readAllBytes(file.toPath()));
+    }
+}
--- src/test/java/example/WeatherResponseTest.java
+++ src/test/java/example/WeatherResponseTest.java
@@ -1,6 +1,5 @@
 package example;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,54 +13,11 @@ import static org.junit.Assert.assertThat;
 public class WeatherResponseTest {
     @Test
     public void shouldDeserializeJson() throws Exception {
-        String jsonResponse = "{\n" +
-                "\"coord\": {\n" +
-                "\"lon\": 145.77,\n" +
-                "\"lat\": -16.92\n" +
-                "},\n" +
-                "\"weather\": [\n" +
-                "{\n" +
-                "\"id\": 802,\n" +
-                "\"main\": \"Clouds\",\n" +
-                "\"description\": \"scattered clouds\",\n" +
-                "\"icon\": \"03n\"\n" +
-                "}\n" +
-                "],\n" +
-                "\"base\": \"stations\",\n" +
-                "\"main\": {\n" +
-                "\"temp\": 300.15,\n" +
-                "\"pressure\": 1007,\n" +
-                "\"humidity\": 74,\n" +
-                "\"temp_min\": 300.15,\n" +
-                "\"temp_max\": 300.15\n" +
-                "},\n" +
-                "\"visibility\": 10000,\n" +
-                "\"wind\": {\n" +
-                "\"speed\": 3.6,\n" +
-                "\"deg\": 160\n" +
-                "},\n" +
-                "\"clouds\": {\n" +
-                "\"all\": 40\n" +
-                "},\n" +
-                "\"dt\": 1485790200,\n" +
-                "\"sys\": {\n" +
-                "\"type\": 1,\n" +
-                "\"id\": 8166,\n" +
-                "\"message\": 0.2064,\n" +
-                "\"country\": \"AU\",\n" +
-                "\"sunrise\": 1485720272,\n" +
-                "\"sunset\": 1485766550\n" +
-                "},\n" +
-                "\"id\": 2172797,\n" +
-                "\"name\": \"Cairns\",\n" +
-                "\"cod\": 200\n" +
-                "}";
+        String jsonResponse = FileLoader.read("classpath:weatherApiResponse.json");
+        WeatherResponse expectedResponse = weatherResponse().description("light intensity drizzle").build();
 
-
-        WeatherResponse expectedResponse = weatherResponse().description("scattered clouds").build();
         WeatherResponse parsedResponse = new ObjectMapper().readValue(jsonResponse, WeatherResponse.class);
+
         assertThat(parsedResponse, is(expectedResponse));
     }
-
-
 }
