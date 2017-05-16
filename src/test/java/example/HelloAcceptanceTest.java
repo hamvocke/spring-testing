3f55e9b58a97dc603a41b19467b7a2ef80b20575
Hermann Vocke <hvocke@thoughtworks.com>
2017-05-16 16:44:13 +0200

    split acceptance tests

--- src/test/java/example/HelloAcceptanceTest.java
+++ src/test/java/example/HelloAcceptanceTest.java
@@ -1,34 +1,25 @@
 package example;
 
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import example.helper.FileLoader;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.core.Is.is;
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class HelloWorldAcceptanceTest {
+public class HelloAcceptanceTest {
 
     @Autowired
-    PersonRepository personRepository;
+    private PersonRepository personRepository;
 
     @LocalServerPort
-    int port;
-
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule(8089);
+    private int port;
 
     @Test
     public void shouldReturnHelloWorld() throws Exception {
@@ -49,21 +40,5 @@ public class HelloWorldAcceptanceTest {
                 .then()
                 .statusCode(is(200))
                 .body(containsString("Hello Peter Pan!"));
-    }
-
-
-    @Test
-    public void shouldReturnYesterdaysWeather() throws Exception {
-        wireMockRule.stubFor(get(urlPathEqualTo("/"))
-                .willReturn(aResponse()
-                        .withBody(FileLoader.read("classpath:weatherApiResponse.json"))
-                        .withHeader(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                        .withStatus(200)));
-
-        when()
-                .get(String.format("http://localhost:%s/yesterdaysWeather", port))
-                .then()
-                .statusCode(is(200))
-                .body(containsString("light intensity drizzle"));
     }
 }
---
+++ src/test/java/example/WeatherAcceptanceTest.java
@@ -0,0 +1,45 @@
+package example;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import example.helper.FileLoader;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class WeatherAcceptanceTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(8089);
+
+    @Test
+    public void shouldReturnYesterdaysWeather() throws Exception {
+        wireMockRule.stubFor(get(urlPathEqualTo("/"))
+                .willReturn(aResponse()
+                        .withBody(FileLoader.read("classpath:weatherApiResponse.json"))
+                        .withHeader(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withStatus(200)));
+
+        when()
+                .get(String.format("http://localhost:%s/yesterdaysWeather", port))
+                .then()
+                .statusCode(is(200))
+                .body(containsString("light intensity drizzle"));
+    }
+}
