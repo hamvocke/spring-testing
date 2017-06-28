3fc56c17af94883049fc23e1e14968a50b2b796c
Hermann Vocke <hvocke@thoughtworks.com>
2017-06-28 16:47:07 +0200

    make functional test run in spring context

--- src/test/java/example/ExampleFunctionalTest.java
+++ src/test/java/example/ExampleFunctionalTest.java
@@ -3,17 +3,25 @@ package example;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ExampleFunctionalTest {
 
-    protected WebDriver driver;
+    private WebDriver driver;
 
+    @LocalServerPort
+    private int port;
 
     @Before
     public void setUp(){
@@ -26,13 +34,10 @@ public class ExampleFunctionalTest {
         driver.close();
     }
 
-
     @Test
     public void helloPageHasTextHelloWorld(){
-
-        driver.get("http://127.0.0.1:8080/hello");
+        driver.get(String.format("http://127.0.0.1:%s/hello", port));
 
         assertThat(driver.findElement(By.tagName("body")).getText(), containsString("Hello World!"));
-
     }
 }
