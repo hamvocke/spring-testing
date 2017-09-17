ae93072048f423e51d20267954a6fae4ba9dc035
Ham Vocke <hermann.vocke@gmail.com>
2017-09-17 14:48:23 +0200

    implement weather client consumer test

---
+++ src/test/java/example/weather/WeatherClientConsumerTest.java
@@ -0,0 +1,55 @@
+package example.weather;
+
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRuleMk2;
+import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.RequestResponsePact;
+import example.helper.FileLoader;
+import org.apache.http.entity.ContentType;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class WeatherClientConsumerTest {
+
+    @Autowired
+    private WeatherClient weatherClient;
+
+    @Rule
+    public PactProviderRuleMk2 weatherProvider = new PactProviderRuleMk2("weather_provider", "localhost", 8089, this);
+
+    @Pact( consumer="test_consumer")
+    public RequestResponsePact createPact(PactDslWithProvider builder) throws IOException {
+        return builder
+                .given("weather forecast data")
+                .uponReceiving("a request for a weather request for Hamburg")
+                    .path("/some-test-api-key/53.5511,9.9937")
+                    .method("GET")
+                .willRespondWith()
+                    .status(200)
+                    .body(FileLoader.read("classpath:weatherApiResponse.json"), ContentType.APPLICATION_JSON)
+                .toPact();
+    }
+
+    @Test
+    @PactVerification("weather_provider")
+    public void shouldFetchWeatherInformation() throws Exception {
+        Optional<WeatherResponse> weatherResponse = weatherClient.fetchWeather();
+        assertThat(weatherResponse.isPresent(), is(true));
+        assertThat(weatherResponse.get().getSummary(), is("Rain"));
+    }
+}
