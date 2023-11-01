5f7e366724ee904b1d62090826f39bd57a5db79e
Ham Vocke <ham@hamvocke.com>
2023-11-01 13:12:40 +0100

    downgrade pact version again
    something's off when we're trying to set a port on the mock server

--- src/test/java/example/person/PersonRepositoryIntegrationTest.java
+++ src/test/java/example/person/PersonRepositoryIntegrationTest.java
@@ -2,10 +2,8 @@ package example.person;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
